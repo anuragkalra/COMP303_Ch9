0ef48102cfc69adcf4b1801a1d08aa773e10e64e
anuragkalra <anuragkalra@outlook.com>
2016-10-25 18:12:39 -0400

    set up Producer class

---
+++ COMP303_Ch9/src/Producer.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * An action that repeatedly inserts a greeting into a queue
+ *
+ */
+public class Producer implements Runnable{
+
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
