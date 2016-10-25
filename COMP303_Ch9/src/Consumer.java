351260894e6ae7c0a5465b1ec113ec57a5dcc2f9
anuragkalra <anuragkalra@outlook.com>
2016-10-25 19:18:39 -0400

    constructed Consumer class

--- COMP303_Ch9/src/Consumer.java
+++ COMP303_Ch9/src/Consumer.java
@@ -3,6 +3,33 @@
  * An action that repeatedly removes a greeting from a queue
  *
  */
-public class Consumer {
-
+public class Consumer implements Runnable {
+	private BoundedQueue<String> queue;
+	private int greetingCount;
+	private static final int DELAY = 10;
+	
+	/**
+	 * Constructs a consumer object
+	 * @param aQueue
+	 * @param count
+	 */
+	public Consumer(BoundedQueue<String> aQueue, int count){
+		queue = aQueue;
+		greetingCount = count;
+	}
+	
+	public void run(){
+		try{
+			int i = 1;
+			while ( i <= greetingCount){
+				String greeting = queue.remove();
+				System.out.println(greeting);
+				i++;
+				Thread.sleep((int) (Math.random() * DELAY));
+			}
+		}
+		catch(InterruptedException e){
+			
+		}
+	}
 }
